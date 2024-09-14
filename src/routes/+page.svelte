0d05720bb41c0a369d6a6638d09164abf63118ce
TM-WebD3V <yasuounforgiven779@gmail.com>
2024-09-14 07:47:30 +0200

    added logos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
+<script lang="ts">
+  const images = [
+    { src: "/brands/bekament.png", alt: "Bekament" },
+    { src: "/brands/knauf.png", alt: "Knauf" },
+    { src: "/brands/Philips.png", alt: "Philips" },
+    { src: "/brands/rigips.png", alt: "rigips" },
+    { src: "/brands/Tarkett.png", alt: "Tarkett" },
+  ];
+</script>
+
 <div class="bg-slate-300 min-h-screen">
   <h1 class="text-5xl text-center py-10">
     Sedra za sve vase gradevinske potvrde
   </h1>
+  <div class="flex justify-center align-center gap-4">
+    {#each images as image}
+      <img class=" w-32" src={image.src} alt={image.alt} />
+    {/each}
+  </div>
 </div>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <main>
-  <Card.Root>
+  <Card.Root class="max-w-2xl mx-auto">
     <Card.Header>
       <Card.Title>Kontakt Informacije</Card.Title>
     </Card.Header>
     <Card.Content>
       <Phone fill="#333"></Phone>
-      <p>Pozovite nas na:<a href="tel:+38761069798">061 069 798</a></p>
+      <p>Pozovite nas na: <a href="tel:+38761069798"> 061 069 798</a></p>
     </Card.Content>
   </Card.Root>
 </main>
