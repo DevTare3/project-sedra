776cb8998f7d544ac11774a8bf11e58b8190f5e0
TM-WebD3V <yasuounforgiven779@gmail.com>
2024-09-15 12:31:20 +0200

    added location and map pins

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,12 @@
 </script>
 
 <div class="bg-slate-300 min-h-screen">
-  <h1 class="text-5xl text-center py-10">
+  <h1 class="text-5xl text-center py-10 px-4">
     Sedra za sve vase gradevinske potvrde
   </h1>
-  <div class="flex justify-center align-center gap-4">
-    {#each images as image}
-      <img class=" w-32" src={image.src} alt={image.alt} />
-    {/each}
-  </div>
+</div>
+<div class="flex justify-center align-center gap-4 bg-black py-8 px-16">
+  {#each images as image}
+    <img class="min-w-[4rem] min-h-[4rem]" src={image.src} alt={image.alt} />
+  {/each}
 </div>
---
+++ src/routes/location/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { MapPinned } from "lucide-svelte";
+</script>
+
+<main class="px-4">
+  <h1 class="text-4xl">Gdje se nalazimo?</h1>
+  <p>
+    Nalazimo se samo minutu od glavne ceste u kovacima<a
+      class="hover:text-red-500 transition-all visited:text-black flex gap-2"
+      href="https://maps.app.goo.gl/HFMTtyXdTgSUAY4L7"
+      target="_blank"
+    >
+      Kliknite za lokaciju na mapi!<MapPinned />
+    </a>
+  </p>
+</main>
