b5cde5c258d765b162a84bbfffe43f15295d301b
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2024-09-19 16:25:59 +0200

    adapter

--- src/routes/location/+page.svelte
+++ src/routes/location/+page.svelte
@@ -2,11 +2,11 @@
   import { MapPinned } from "lucide-svelte";
 </script>
 
-<main class="px-4 bg-orange-400 min-h-screen text-center py-8">
+<main class="px-4 bg-blue-500 min-h-screen text-center py-8">
   <h1 class="text-4xl mb-4">Gdje se nalazimo?</h1>
   <p class="text-center">
     Nalazimo se samo minutu od glavne ceste u kovacima<a
-      class="hover:text-orange-500 transition-all visited:text-white flex gap-2 text-white items-center justify-center"
+      class="hover:text-blue-800 transition-all visited:text-white flex gap-2 text-white items-center justify-center"
       href="https://maps.app.goo.gl/HFMTtyXdTgSUAY4L7"
       target="_blank"
     >
