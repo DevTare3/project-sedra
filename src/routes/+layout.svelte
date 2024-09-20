fef35ef6e84aeaff6bf8085322394a1db4c46bc0
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2024-09-20 12:31:16 +0200

    i am speed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button";
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+  injectSpeedInsights();
   import "../app.css";
 </script>
 
